44b82c6ea14332840a3827acfe746edf8f0c992a
kshi tij <44705390+kshitij5@users.noreply.github.com>
2020-10-05 13:14:31 +0530

    Refactored the requested Changes on SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'Screens/bottom_navigation.dart';
+import 'Screens/SplashScreen.dart';
 
 void main() {
   runApp(covidCare());
@@ -10,16 +9,5 @@ class covidCare extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: SplashScreen(),debugShowCheckedModeBanner: false,);
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      splash: 'assets/images/splash.png',
-      nextScreen: BottomNavScreen(),
-      splashTransition: SplashTransition.scaleTransition,
-    );
   }
 }
